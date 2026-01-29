cf7401a4194803b7adfcfcf60dd10c68402c5671
nuuuwan <nuuuwan@gmail.com>
2026-01-29 14:02:04 +0530

    Add react-router-dom for routing and implement HomePage in App component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import HomePage from "./view/pages/HomePage";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#1976d2",
+    },
+    secondary: {
+      main: "#dc004e",
+    },
+  },
+});
+
 function App() {
-  return "TODO";
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
+  );
 }
 
 export default App;
