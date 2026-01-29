07713527f404227433622ef9afd59cb2c5d6272b
nuuuwan <nuuuwan@gmail.com>
2026-01-29 14:08:53 +0530

    Add MapEventHandler to update location on map movement and navigate to new coordinates

--- src/view/moles/MapView.js
+++ src/view/moles/MapView.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from "react";
-import { MapContainer, TileLayer, useMap } from "react-leaflet";
+import { MapContainer, TileLayer, useMap, useMapEvents } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { Box } from "@mui/material";
 import LatLng from "../../nonview/base/LatLng";
@@ -16,7 +16,19 @@ function MapCenterController({ latLng }) {
   return null;
 }
 
-export default function MapView({ latLng = LatLng.DEFAULT }) {
+function MapEventHandler({ onLatLngChange }) {
+  const map = useMapEvents({
+    moveend: () => {
+      const center = map.getCenter();
+      const newLatLng = new LatLng(center.lat, center.lng);
+      onLatLngChange(newLatLng);
+    },
+  });
+
+  return null;
+}
+
+export default function MapView({ latLng = LatLng.DEFAULT, onLatLngChange }) {
   const position = latLng.toArray();
 
   return (
@@ -31,6 +43,7 @@ export default function MapView({ latLng = LatLng.DEFAULT }) {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         <MapCenterController latLng={latLng} />
+        {onLatLngChange && <MapEventHandler onLatLngChange={onLatLngChange} />}
       </MapContainer>
     </Box>
   );
--- src/view/pages/HomePage.js
+++ src/view/pages/HomePage.js
@@ -1,18 +1,24 @@
 import React, { useState } from "react";
 import { Box } from "@mui/material";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import MapView from "../moles/MapView";
 import DetailsView from "../moles/DetailsView";
 import LatLng from "../../nonview/base/LatLng";
 
 export default function HomePage() {
   const { lat, lng } = useParams();
+  const navigate = useNavigate();
 
   // Initialize latLng from URL params or use default
   const initialLatLng =
     lat && lng ? new LatLng(parseFloat(lat), parseFloat(lng)) : LatLng.DEFAULT;
 
   const [latLng, setLatLng] = useState(initialLatLng);
+
+  const handleLatLngChange = (newLatLng) => {
+    setLatLng(newLatLng);
+    navigate(`/locator/${newLatLng.lat}/${newLatLng.lng}`, { replace: true });
+  };
 
   return (
     <Box
@@ -31,7 +37,7 @@ export default function HomePage() {
           width: "100%",
         }}
       >
-        <MapView latLng={latLng} />
+        <MapView latLng={latLng} onLatLngChange={handleLatLngChange} />
       </Box>
 
       {/* Details Display - Bottom Half */}
