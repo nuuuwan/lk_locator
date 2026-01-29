d9ac3ab77caede5f82ddbd1a98a7bb29f9964037
nuuuwan <nuuuwan@gmail.com>
2026-01-29 15:36:39 +0530

    Replace Typography with Alert in LocationNotFound component for improved user feedback

--- src/view/atoms/LocationNotFound.js
+++ src/view/atoms/LocationNotFound.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { Typography } from "@mui/material";
+import { Alert } from "@mui/material";
 
 export default function LocationNotFound({
   message = "Location not found in any province",
 }) {
   return (
-    <Typography variant="body2" color="text.secondary" sx={{ mt: 1 }}>
+    <Alert severity="warning" sx={{ mt: 1 }}>
       {message}
-    </Typography>
+    </Alert>
   );
 }
