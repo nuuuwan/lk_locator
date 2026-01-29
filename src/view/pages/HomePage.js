bf9d6e92b15f847c5eb19c2781d30e1d5231bc04
nuuuwan <nuuuwan@gmail.com>
2026-01-29 17:14:56 +0530

    Fix position of DetailsView in HomePage to align with layout changes

--- src/view/pages/HomePage.js
+++ src/view/pages/HomePage.js
@@ -31,7 +31,7 @@ function HomePageContent() {
         sx={{
           position: "fixed",
           top: 0,
-          left: 0,
+          right: 0,
           width: 240,
           zIndex: 2000,
         }}
