af8fa924e6a6f6574877e6088af4b8eb32f83c38
nuuuwan <nuuuwan@gmail.com>
2026-01-29 15:32:14 +0530

    Remove zoom control from MapContainer in MapView component

--- src/view/moles/MapView.js
+++ src/view/moles/MapView.js
@@ -50,6 +50,7 @@ export default function MapView({
       <MapContainer
         center={position}
         zoom={12}
+        zoomControl={false}
         style={{ width: "100%", height: "100%" }}
       >
         <TileLayer
