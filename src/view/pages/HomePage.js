d82f4f47fbc952ee27253248c167d12fbc2aedfb
nuuuwan <nuuuwan@gmail.com>
2026-01-29 14:06:30 +0530

    Add state management for latitude and longitude in HomePage component

--- src/view/pages/HomePage.js
+++ src/view/pages/HomePage.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box } from "@mui/material";
+import { useParams } from "react-router-dom";
 import MapView from "../moles/MapView";
 import DetailsView from "../moles/DetailsView";
+import LatLng from "../../nonview/base/LatLng";
 
 export default function HomePage() {
+  const { lat, lng } = useParams();
+
+  // Initialize latLng from URL params or use default
+  const initialLatLng =
+    lat && lng ? new LatLng(parseFloat(lat), parseFloat(lng)) : LatLng.DEFAULT;
+
+  const [latLng, setLatLng] = useState(initialLatLng);
+
   return (
     <Box
       sx={{
@@ -21,7 +31,7 @@ export default function HomePage() {
           width: "100%",
         }}
       >
-        <MapView />
+        <MapView latLng={latLng} />
       </Box>
 
       {/* Details Display - Bottom Half */}
@@ -31,7 +41,7 @@ export default function HomePage() {
           width: "100%",
         }}
       >
-        <DetailsView />
+        <DetailsView latLng={latLng} />
       </Box>
     </Box>
   );
