38022bbec05c88a29502daaea45112f2ea846eeb
nuuuwan <nuuuwan@gmail.com>
2026-01-30 14:40:14 +0530

    Add GND class methods for legacy data retrieval and indexing

--- src/nonview/core/GND.js
+++ src/nonview/core/GND.js
@@ -1,4 +1,5 @@
 import AbstractRegion from "./AbstractRegion";
+import WWW from "../base/WWW";
 
 export default class GND extends AbstractRegion {
   static get regionName() {
@@ -7,5 +8,20 @@ export default class GND extends AbstractRegion {
 
   static get regionShortName() {
     return "GND";
+  }
+
+  async getLegacyData() {
+    const idx = await this.constructor.idxLegacy();
+    return idx[this.id] || null;
+  }
+
+  static async idxLegacy() {
+    const url =
+      "https://raw.githubusercontent.com" +
+      "/nuuuwan/gig-data/refs/heads/master/ents/gnd.tsv";
+
+    const data = await WWW.fetchTSV(url);
+    const idx = Object.fromEntries(data.map((d) => [d.gnd_id, d]));
+    return idx;
   }
 }
