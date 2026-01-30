7ebe2ca40ffc2c2b8a6d57cb317c1b3d2e452139
nuuuwan <nuuuwan@gmail.com>
2026-01-30 14:52:13 +0530

    Add instruction Snackbar to HomePage for user guidance

--- src/view/pages/HomePage.js
+++ src/view/pages/HomePage.js
@@ -1,19 +1,38 @@
-import React, { useEffect } from "react";
-import { Box } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { Box, Snackbar, Alert } from "@mui/material";
 import MapView from "../moles/MapView";
 import DetailsView from "../moles/DetailsView";
 import { DataProvider, useData } from "../../nonview/core/DataContext";
 
 function HomePageContent() {
   const { latLng } = useData();
+  const [showInstructions, setShowInstructions] = useState(true);
 
   useEffect(() => {
     // Update document title when latLng changes
     document.title = latLng.toString();
   }, [latLng]);
 
+  const handleCloseInstructions = () => {
+    setShowInstructions(false);
+  };
+
   return (
     <Box sx={{}}>
+      <Snackbar
+        open={showInstructions}
+        autoHideDuration={8_000}
+        onClose={handleCloseInstructions}
+        anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+      >
+        <Alert onClose={handleCloseInstructions} severity="info" sx={{ mt: 2 }}>
+          Drag to anywhere on the map to see which provinces, districts,
+          divisional secretariats, grama niladhari divisions, electoral
+          districts, polling divisions and local authorities in Sri Lanka the
+          location belongs to.
+        </Alert>
+      </Snackbar>
+
       <Box
         sx={{
           position: "fixed",
