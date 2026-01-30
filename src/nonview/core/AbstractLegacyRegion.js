2bcf48038e4dcaadb286663fb59f9ad38b0d0f71
nuuuwan <nuuuwan@gmail.com>
2026-01-30 14:39:56 +0530

    Add AbstractLegacyRegion class to handle API data conversion and URL generation

---
+++ src/nonview/core/AbstractLegacyRegion.js
@@ -0,0 +1,25 @@
+import AbstractRegion from "./AbstractRegion";
+import LatLng from "../base/LatLng";
+
+export default class AbstractLegacyRegion extends AbstractRegion {
+  static fromAPIObject(apiData) {
+    // Convert API format to internal format
+    const centroid = JSON.parse(apiData.centroid);
+    return this.fromObject({
+      id: apiData.id,
+      name: apiData.name,
+      nameSi: undefined,
+      nameTa: undefined,
+      areaSqKm: undefined,
+      centerLatLng: new LatLng(centroid[0], centroid[1]),
+    });
+  }
+
+  static get url() {
+    const regionShortName = this.regionShortName.toLowerCase();
+    return (
+      `https://raw.githubusercontent.com` +
+      `/nuuuwan/gig-data/refs/heads/master/ents/${regionShortName}.tsv`
+    );
+  }
+}
